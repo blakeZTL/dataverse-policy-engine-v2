763a290e4175d12e5cc2897c1e521ea21265bf4a
Blake Bradford <james.b.bradford@faa.gov>
2026-02-14 11:32:04 -0500

    Add enums and option set mapping for policy engine
    Introduced OperatorType, PolicyType, and ValueType enums in DataversePolicyEngine.Core.Model. Added OptionSetMap static class to map these enums to Dataverse option set values and provide conversion methods. This establishes strong typing and clear mapping for policy engine option sets.

---
+++ src/server/DataversePolicyEngine.Core/Model/ValueType.cs
@@ -0,0 +1,11 @@
+﻿namespace DataversePolicyEngine.Core.Model
+{
+    public enum ValueType
+    {
+        String = 0,
+        Number = 1,
+        Boolean = 2,
+        OptionSet = 3,
+        Lookup = 4
+    }
+}

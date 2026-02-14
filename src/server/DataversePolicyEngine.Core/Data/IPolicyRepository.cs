9b5bcefa73390edc30c97a6dc6c0cf1d39ec0862
Blake Bradford <james.b.bradford@faa.gov>
2026-02-14 11:31:02 -0500

    Add policy repository interface and implementation
    Introduced IPolicyRepository interface for retrieving policy rules and conditions from Dataverse. Added PolicyRepository class with methods to fetch rules and conditions based on entity, attribute, and rule criteria.

---
+++ src/server/DataversePolicyEngine.Core/Data/IPolicyRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+
+namespace DataversePolicyEngine.Core.Data
+{
+    public interface IPolicyRepository
+    {
+        List<Entity> GetRules(
+            IOrganizationService service,
+            string entityLogicalName,
+            string attributeLogicalName
+        );
+        List<Entity> GetConditions(IOrganizationService service, Guid ruleId);
+    }
+}

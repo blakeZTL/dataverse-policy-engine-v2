20426990dc763ecfa9bc1124e0c8620f251b9401
Blake Bradford <james.b.bradford@faa.gov>
2026-02-14 21:39:15 -0500

    Add batch response/result classes and ClickOnce settings
    Added EvaluatePoliciesBatchResponse and EvaluatePoliciesTargetResult data contract classes for batch policy evaluation results. Updated project file with ClickOnce deployment settings and bootstrapper package references for .NET Framework 4.6.2 and 3.5 SP1.

---
+++ src/server/DataversePolicyEngine.CustomApi/Messages/EvaluatePoliciesBatchResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace DataversePolicyEngine.CustomApi.Messages
+{
+    [DataContract]
+    public class EvaluatePoliciesBatchResponse
+    {
+        [DataMember]
+        public List<EvaluatePoliciesTargetResult> Results { get; set; } =
+            new List<EvaluatePoliciesTargetResult>();
+    }
+}
---
+++ src/server/DataversePolicyEngine.CustomApi/Messages/EvaluatePoliciesTargetResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataversePolicyEngine.CustomApi.Messages
+{
+    [DataContract]
+    public class EvaluatePoliciesTargetResult
+    {
+        [DataMember]
+        public string Target { get; set; }
+
+        [DataMember]
+        public bool Visible { get; set; }
+
+        [DataMember]
+        public bool Required { get; set; }
+
+        [DataMember]
+        public bool NotAllowed { get; set; }
+    }
+}
